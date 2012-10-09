920ac09aa61861dba5c4b2456371a665716bb1c1
taraymovich@gmail.com <taraymovich@gmail.com@bbc9809c-7da1-418a-8ed1-f0bb0a5a736a>
2012-10-09 18:02:46 +0000

    Importing HeartRateMonitor
    git-svn-id: https://subversion.assembla.com/svn/heartratemonitor@6 bbc9809c-7da1-418a-8ed1-f0bb0a5a736a

---
+++ trunk/iPhoneApp/HeartRateMonitor/HeartRateMonitor/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  HeartRateMonitor
+//
+//  Created by Alexander O. Taraymovich on 07.10.12.
+//  Copyright (c) 2012 Alexander O. Taraymovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ trunk/iPhoneApp/HeartRateMonitor/HeartRateMonitorTests/HeartRateMonitorTests.m
@@ -0,0 +1,32 @@
+//
+//  HeartRateMonitorTests.m
+//  HeartRateMonitorTests
+//
+//  Created by Alexander O. Taraymovich on 07.10.12.
+//  Copyright (c) 2012 Alexander O. Taraymovich. All rights reserved.
+//
+
+#import "HeartRateMonitorTests.h"
+
+@implementation HeartRateMonitorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HeartRateMonitorTests");
+}
+
+@end
