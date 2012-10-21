3596213dfae992bb1772c4a3ca4d77e69557aa53
taraymovich@gmail.com <taraymovich@gmail.com@bbc9809c-7da1-418a-8ed1-f0bb0a5a736a>
2012-10-21 21:30:38 +0000

    Importing HeartRateMonitor
    git-svn-id: https://subversion.assembla.com/svn/heartratemonitor@22 bbc9809c-7da1-418a-8ed1-f0bb0a5a736a

---
+++ trunk/HeartRateMonitor/HeartRateMonitor/GraphViewController.m
@@ -0,0 +1,17 @@
+//
+//  GraphViewController.m
+//  HeartRateMonitor
+//
+//  Created by Alexander O. Taraymovich on 12.10.12.
+//  Copyright (c) 2012 Alexander O. Taraymovich. All rights reserved.
+//
+
+#import "GraphViewController.h"
+
+@interface GraphViewController ()
+
+@end
+
+@implementation GraphViewController
+
+@end
