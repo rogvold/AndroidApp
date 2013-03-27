75bf2b52267ade91a2d999ba1aa07294bfe46758
pogr.yuo <pogr.yuo@bbc9809c-7da1-418a-8ed1-f0bb0a5a736a>
2013-03-27 22:30:14 +0000

    ClientServerInteraction updated to work with new API
    git-svn-id: https://subversion.assembla.com/svn/heartratemonitor@187 bbc9809c-7da1-418a-8ed1-f0bb0a5a736a

---
+++ trunk/ClientServerInteraction/Error/ServerResponseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientServerInteraction.Error
+{
+    public class ServerResponseException : Exception
+    {
+        public ServerResponseException() : base() { }
+        public ServerResponseException(string message) : base(message) { }
+        public ServerResponseException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ trunk/ClientServerInteraction/Error/TaskErrorHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ClientServerInteraction.Error
+{
+    public class TaskErrorHelper
+    {
+        internal static Task ErrorAsync(Exception e)
+        {
+            return ErrorAsync<object>(e);
+        }
+
+        internal static Task<T> ErrorAsync<T>(Exception e)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            tcs.SetException(e);
+            return tcs.Task;
+        }
+
+
+    }
+}
