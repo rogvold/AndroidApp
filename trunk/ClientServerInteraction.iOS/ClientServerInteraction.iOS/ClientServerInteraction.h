52eb9c45a714ce319aa1e00410f43fcd270380a1
taraymovich <taraymovich@bbc9809c-7da1-418a-8ed1-f0bb0a5a736a>
2013-04-01 09:51:45 +0000

    Importing ClientServerInteraction.iOS
    git-svn-id: https://subversion.assembla.com/svn/heartratemonitor@194 bbc9809c-7da1-418a-8ed1-f0bb0a5a736a

---
+++ trunk/ClientServerInteraction.iOS/ClientServerInteraction.iOS/ClientServerInteraction.h
@@ -0,0 +1,14 @@
+//
+//  ClientServerInteraction.h
+//  ClientServerInteraction
+//
+//  Created by Alexander O. Taraymovich on 01.04.13.
+//  Copyright (c) 2013 Alexander O. Taraymovich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <RestKit/RestKit.h>
+
+@interface ClientServerInteraction : NSObject
+
+@end
---
+++ trunk/ClientServerInteraction.iOS/ClientServerInteraction.iOS/ClientServerInteraction.m
@@ -0,0 +1,14 @@
+//
+//  ClientServerInteraction.m
+//  ClientServerInteraction
+//
+//  Created by Alexander O. Taraymovich on 01.04.13.
+//  Copyright (c) 2013 Alexander O. Taraymovich. All rights reserved.
+//
+
+#import "ClientServerInteraction.h"
+#import <RestKit/RestKit.h>
+
+@implementation ClientServerInteraction
+
+@end
