d7593219de2d3d8f942f20f4d9f00a76f8514f2e
pogr.yuo <pogr.yuo@bbc9809c-7da1-418a-8ed1-f0bb0a5a736a>
2013-04-11 13:16:54 +0000

    AccessToken.cs added
    git-svn-id: https://subversion.assembla.com/svn/heartratemonitor@208 bbc9809c-7da1-418a-8ed1-f0bb0a5a736a

---
+++ trunk/ClientServerInteraction/AccessToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientServerInteraction
+{
+    public class AccessToken
+    {
+        public string Token { get; set; }
+        public long ExpiredDate { get; set; }
+    }
+}
