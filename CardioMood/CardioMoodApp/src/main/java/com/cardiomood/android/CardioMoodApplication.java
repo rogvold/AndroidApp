c54ba7fa42d489f03ef13f0fd2a72b86e77a0e27
Anton Danshin <anton.danshin@frtk.ru>
2014-04-27 18:19:11 +0400

    some more

--- CardioMood/CardioMoodApp/src/main/java/com/cardiomood/android/CardioMoodApplication.java
+++ CardioMood/CardioMoodApp/src/main/java/com/cardiomood/android/CardioMoodApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.cardiomood.android.db.HeartRateDBHelper;
+import com.parse.Parse;
 
 /**
  * Created by danshin on 31.10.13.
@@ -15,6 +16,7 @@ public class CardioMoodApplication extends Application {
     public void onCreate() {
         super.onCreate();
         database = new HeartRateDBHelper(this).getWritableDatabase();
+        Parse.initialize(this, "D00uihFFqj0K9yAoecTzR5t4VxJeSGfYOee4LciN", "vaqte7MiPMce9h4HFCnmTnkieIOarA9WPoCcxVnk");
     }
 
     @Override
