8511194a62d68f8e37e94fa5287e8fc1749944e4
Anton Danshin <anton.danshin@frtk.ru>
2014-07-14 13:04:30 +0400

    Migration to AS 0.8 and target android L: attempt #1

---
+++ CardioMood/AndroidControls/src/main/java/com/cardiomood/android/controls/gauge/NeedleGauge.java
@@ -0,0 +1,25 @@
+package com.cardiomood.android.controls.gauge;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by danon on 11.07.2014.
+ */
+public class NeedleGauge extends View {
+
+    public NeedleGauge(Context context) {
+        super(context);
+    }
+
+    public NeedleGauge(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NeedleGauge(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
